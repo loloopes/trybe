efb1945e3fa3eeb830769671a3e106b3283f0023
Lopes <gus.lcampos38@gmail.com>
2021-05-27 19:08:10 -0300

    Exercicio DOM manipulacao js

---
+++ Bloco5/5_1/script.js
@@ -0,0 +1,3 @@
+document.querySelector('#header-container').style.color ='White';
+document.querySelector('#header-container').style.backgroundColor ='green';
+document.querySelector('#header-container').style.textAlign = 'center';
--- Bloco5/temp.js
+++
@@ -1,8 +0,0 @@
-
-
- const paragraph = document.getElementById("paragraph");
- paragraph.style.color = "red";
-
-
-//  const paragraph = document.getElementById("second-paragraph");
-//  second-paragraph.style.color = "blue";
